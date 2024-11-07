6dd80648817da01630fb3028ee1dd0c558641653
DilanFdo <priyankarad276@gmail.com>
2024-11-07 12:26:54 +0530

    task added

--- task API1/Controllers/TasksController.cs
+++ task API1/Controllers/TasksController.cs
@@ -10,7 +10,7 @@ namespace task_API1.Controllers
         [HttpGet]
         public IActionResult Tasks()
         {
-            var tasks = new string[] { "task1", "task2", "task3" };
+            var tasks = new string[] { "task1", "task2", "task3", "task4" };
             return Ok(tasks);
         }
 
